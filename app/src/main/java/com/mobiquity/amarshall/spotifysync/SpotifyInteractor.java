9a02668f034e2af9a3c0fd2569d19a48d0a53f8b
Jared Fowler <jfowler@mobiquityinc.com>
2016-03-24 16:34:43 -0400

    Adding socket tests

---
+++ app/src/main/java/com/mobiquity/amarshall/spotifysync/SpotifyInteractor.java
@@ -0,0 +1,23 @@
+package com.mobiquity.amarshall.spotifysync;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.SavedTrack;
+import retrofit.Callback;
+
+/**
+ * Created by jfowler on 3/23/16.
+ */
+public class SpotifyInteractor {
+
+    private SpotifyApi spotifyApi;
+
+    public SpotifyInteractor(String accessToken) {
+        spotifyApi = new SpotifyApi();
+        spotifyApi.setAccessToken(accessToken);
+    }
+
+    public void getUsersSavedTracks(Callback<Pager<SavedTrack>> callback) {
+        spotifyApi.getService().getMySavedTracks(callback);
+    }
+}
